c379bc87896227dc531ceb5827acfc25560dbc9d
barakaramadhan <barakaramadhan444@gmail.com>
2025-02-04 10:07:40 +0700

    update

---
+++ Card Shipping/script.js
@@ -0,0 +1,5 @@
+function pertambahan(pertambahan) {
+    let getElement = document.getElementById("value").innerHTML;
+    let total = getElement +  pertambahan;
+    document.getElementById("value").innerHTML = total;
+  }
